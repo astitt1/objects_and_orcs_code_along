0ebb545731782ea0fce136712c29869136201348
Ariel Stitt <astitt@perscholas.org>
2023-06-14 11:23:17 -0400

    created an object

---
+++ app.js
@@ -0,0 +1,10 @@
+//USe array inside an object
+
+const adventurer = {
+    name: "Azreal",
+    hitPoint: 10,
+    belongings: ["staff", "potions", "medicine"]
+}
+
+//access the values in the array
+
