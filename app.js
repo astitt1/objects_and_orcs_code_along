3164af6ff889e4414d0bc3903b11aae2246be328
Ariel Stitt <astitt@perscholas.org>
2023-06-14 11:34:13 -0400

    objects withing objects

--- app.js
+++ app.js
@@ -3,8 +3,29 @@
 const adventurer = {
     name: "Azreal",
     hitPoint: 10,
-    belongings: ["staff", "potions", "medicine"]
+    belongings: ["staff", "potions", "medicine"],
+    companion: {
+        name: "stinky",
+        type: "Cat"
+    }
 }
 
+console.log(`My companions name is ${adventurer.companion.name}`);
+
 //access the values in the array
+
+// console.log(adventurer.belongings)
+
+//access the first belonging
+
+// console.log(` first belonging: ${adventurer.belongings[0]}`);
+
+//iterate over an array inside of an object
+
+// for(let i = 0; i < adventurer.belongings.length; i++){
+//     console.log(adventurer.belongings[i])
+// }
+
+// #1 OBJ within OBJ
+
 
