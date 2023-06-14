f1b26be78fb7c29f86413773d5957b306555f733
Ariel Stitt <astitt@perscholas.org>
2023-06-14 11:45:21 -0400

    looping through array of objects

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-//USe array inside an object
+// //USe array inside an object
 
-const adventurer = {
-    name: "Azreal",
-    hitPoint: 10,
-    belongings: ["staff", "potions", "medicine"],
-    companion: {
-        name: "stinky",
-        type: "Cat"
-    }
-}
+// const adventurer = {
+//     name: "Azreal",
+//     hitPoint: 10,
+//     belongings: ["staff", "potions", "medicine"],
+//     companion: {
+//         name: "stinky",
+//         type: "Cat",
+//         belongings: ["ball of yarn", "healing potions", "health insurance", "satchel of elements"]
+//     }
+// }
 
-console.log(`My companions name is ${adventurer.companion.name}`);
+// console.log(`My companions name is ${adventurer.companion.name}`);
+// console.log(`One of my companions belongings is ${adventurer.companion.belongings[2]}`);
 
 //access the values in the array
 
@@ -28,4 +30,24 @@ console.log(`My companions name is ${adventurer.companion.name}`);
 
 // #1 OBJ within OBJ
 
+
+// const movies = [ 
+//     { 
+//         title: "Tokyo Story" 
+//     },  
+//     { 
+//         title: "Paul Blart: Mall Cop" 
+//     }, 
+//     { 
+//         title: "L'Avventura" 
+//     } 
+// ];
+
+// console.log(movies[0].title) //object
+
+// // Looping over array of objects
+
+// for(let i = 0; i < movies.length; i++) {
+//     console.log(movies[i].title);
+// }
 
